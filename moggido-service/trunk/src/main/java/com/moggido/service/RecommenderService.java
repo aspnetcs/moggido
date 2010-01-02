ec410c2fefd26f3d851dab135b392a7075dbcb3f
carloscadete <carloscadete@cb333dde-e8fd-11de-a21f-59b1e5349453>
2010-01-02 14:31:59 +0000

    Implementation of a Simple Recomendation using SlopeOne Mahout Implementation

--- moggido-service/trunk/src/main/java/com/moggido/service/RecommenderService.java
+++ moggido-service/trunk/src/main/java/com/moggido/service/RecommenderService.java
@@ -1,5 +1,10 @@
 package com.moggido.service;
 
+import java.util.List;
+
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+
 public interface RecommenderService {
 
+    List<RecommendedItem> getRecommendedItems(long userID, int howMany);
 }
--- moggido-service/trunk/src/main/java/com/moggido/service/impl/MoggidoRecommenderService.java
+++ moggido-service/trunk/src/main/java/com/moggido/service/impl/MoggidoRecommenderService.java
@@ -1,33 +1,61 @@
 package com.moggido.service.impl;
 
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+
+import com.moggido.engine.cf.taste.MoggidoDataModel;
+import com.moggido.engine.cf.taste.MoggidoRecommender;
 import com.moggido.service.RecommenderService;
 
+/**
+ * API to expose
+ *
+ * @author ccadete
+ *
+ */
 public class MoggidoRecommenderService implements RecommenderService {
 
-    //    @GET
-    //    @Path("/{apikey}")
-    //    public void addRating(@PathParam("apikey") String apikey, @PathParam("userId") long userId,
-    //        @PathParam("itemId") long itemId, @PathParam("rating") int rating) {
-    //
-    //    }
+    private MoggidoRecommender recommender;
 
-    public void addItem(String apikey, long itemId) {
-        System.out.println("item:" + itemId);
+    public MoggidoRecommenderService() {
+        try {
+            recommender = new MoggidoRecommender(new MoggidoDataModel(new File("taste.u1.test")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void addUser(String apikey, long userId) {
-        System.out.println("user:" + userId);
-    }
-
-    //    @GET
-    //    @Path("/")
-    //    public int getRating(long userID, long itemID) {
-    //        return 33;
+    //    public void addRating(String apikey, long userId, long itemId, int rating) {
+    //        System.out.println("NOT USED: addRating");
     //    }
     //
-    //    @GET
-    //    @Path("/")
-    //    public long[] getRecommendedItems(long userID, long itemID) {
-    //        return new long[] {1, 2, 3};
+    //    public void addItem(String apikey, long itemId) {
+    //        System.out.println("NOT USED: addItem");
     //    }
+    //
+    //    public void addUser(String apikey, long userId) {
+    //        System.out.println("NOT USED: addUser");
+    //    }
+
+    public List<RecommendedItem> getRecommendedItems(long userID, int howMany) {
+        try {
+            return recommender.recommend(userID, howMany);
+        } catch (TasteException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        RecommenderService service = new MoggidoRecommenderService();
+
+        List<RecommendedItem> items = service.getRecommendedItems(474, 10);
+
+        for (RecommendedItem item : items) {
+            System.out.println(item);
+        }
+    }
 }
